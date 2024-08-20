8e90c6269c41259099ae7d5671a3c5ce45781850
sureplug018 <ezea5149@gmail.com>
2024-08-20 20:22:01 +0100

    edit products admin

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ dotenv.config({ path: 'config.env' });
 const app = require('./app');
 const cronJob = require('./cronJob');
 
-const DB = process.env.DATABASE
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// const DB = process.env.DATABASE
 
 // Trust only the X-Forwarded-For header
 app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
